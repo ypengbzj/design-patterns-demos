a1c28eb104a1ee403aeff118eae4515438d26ac1
Peng <peng_ios@163.com>
2020-10-11 11:14:37 +0800

    add demo

---
+++ demo/2_策略模式/2_策略模式/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  2_策略模式
+//
+//  Created by 于鹏 on 2020/10/11.
+//  Copyright © 2020 ypeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ demo/2_策略模式/2_策略模式/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2_策略模式
+//
+//  Created by 于鹏 on 2020/10/11.
+//  Copyright © 2020 ypeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
